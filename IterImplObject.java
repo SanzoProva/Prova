e89e054912eec9c483f1542ea9e35463f2dc75f6
SanzoProva <34573530+SanzoProva@users.noreply.github.com>
2018-01-07 18:47:01 +0100

    IterImplObject ultima modifica

--- IterImplObject.java
+++ IterImplObject.java
@@ -39,26 +39,25 @@ class IterImplObject {
 		case 'n':
 			int n = 3;
 			IterImpl.skipFixedBytes(iter, n);
-			return field;
+			break;
 		case '{':
 			c = IterImpl.nextToken(iter);
 			if (c == '"') {
-				iter.unreadByte();
-				field = iter.readString();
-				return cyclomaticComplexity(field, iter);
-			}
-			if (c == '}') {
-				return field; // end of object
+				field = funSupp(field, iter);
+				break;
+			} else if (c == '}') {
+				break; // end of object
 			}
 			throw iter.reportError(READ_OBJECT, "expect \" after {");
 		case ',':
-			field = iter.readString();
-			return cyclomaticComplexity(field, iter);
+			field = funSupp2(field, iter);
+			break;
 		case '}':
-			return field; // end of object
+			break; // end of object
 		default:
 			throw iter.reportError("readObject", "expect { or , or } or n, but found: " + Byte.toString(c).charAt(0));
 		}
+		return field;
 	}
 
 	/**
@@ -75,6 +74,35 @@ class IterImplObject {
 	}
 
 	/**
+	 * 
+	 * @param field
+	 * @param iter
+	 * @return
+	 * @throws IOException
+	 */
+	private static String funSupp(String field, JsonIterator iter) throws IOException {
+		String s = field;
+		iter.unreadByte();
+		s = iter.readString();
+		s = cyclomaticComplexity(s, iter);
+		return s;
+	}
+
+	/**
+	 * 
+	 * @param field
+	 * @param iter
+	 * @return
+	 * @throws IOException
+	 */
+	private static String funSupp2(String field, JsonIterator iter) throws IOException {
+		String s = field;
+		s = iter.readString();
+		s = cyclomaticComplexity(s, iter);
+		return s;
+	}
+
+	/**
 	 * @param iter
 	 * @param cb
 	 * @param attachment
@@ -84,22 +112,27 @@ class IterImplObject {
 	public static boolean readObjectCB(JsonIterator iter, JsonIterator.ReadObjectCallback cb, Object attachment)
 			throws IOException {
 		byte c = IterImpl.nextToken(iter);
+		boolean ret = false;
 		if ('{' == c) {
 			c = IterImpl.nextToken(iter);
-			if ('"' == c) {
-				return subReadObjectCB(iter, cb, attachment);
+			switch (c) {
+			case '"':
+				ret = subReadObjectCB(iter, cb, attachment);
+				break;
+			case '}':
+				ret = true;
+				break;
+			default:
+				throw iter.reportError("readObjectCB", "expect \" after {");
 			}
-			if ('}' == c) {
-				return true;
-			}
-			throw iter.reportError("readObjectCB", "expect \" after {");
-		}
-		if ('n' == c) {
+		} else if ('n' == c) {
 			int n = 3;
 			IterImpl.skipFixedBytes(iter, n);
-			return true;
+			ret = true;
+		} else {
+			throw iter.reportError("readObjectCB", "expect { or n");
 		}
-		throw iter.reportError("readObjectCB", "expect { or n");
+		return ret;
 	}
 
 	/**
